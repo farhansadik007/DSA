5958febcc5f141711b5d0ecb3cce202655a82cae
farhansadik007 <sadik.zisan@gmail.com>
2024-11-20 02:43:40 +0600

    linear search

---
+++ linearSearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int linear_search(int A[], int n, int x);
+
+int main() {
+    int arr[8] = {1,2,3,6,7,8,12,13};
+
+    printf("%d", linear_search(arr, 8, 5));
+    //returns the index number if found
+    //otherwise returns -1 if a number isn't found
+
+}
+
+// A[] is the array
+// n is the total elements of array
+// x is the number to search
+int linear_search(int A[], int n, int x) {
+    int i;
+
+    for(i = 0; i < n; i++) {
+        if(A[i] == x) {
+            return i;
+        }
+    }
+
+    return -1;
+}
